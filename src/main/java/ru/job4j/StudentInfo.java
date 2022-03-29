8aa87a3c6aa397d7eec765e7bb58f9fc8769ec76
Armen <armen93martirosyan@gmail.com>
2022-03-29 13:11:28 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Armen Martirosyan");
+        System.out.println("20.11.1993");
+
+
+    }
+}
